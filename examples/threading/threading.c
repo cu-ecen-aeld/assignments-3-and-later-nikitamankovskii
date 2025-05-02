540ec6e67aa6cf33fe1da91aa2791116c33a767d
nikita.mankovskii <nikita.mankovskii@tii.ae>
2025-05-02 16:46:07 +0400

    assignment4

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,30 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    DEBUG_LOG("Startint.")
+    usleep(thread_func_args->wait_to_obtain_ms*1000);
+    DEBUG_LOG("Waint to obtaint.")
+
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+    if (rc != 0) {
+        ERROR_LOG("Fail. Obtaing mutex");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    
+    usleep(thread_func_args->wait_to_release_ms*1000);
+
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    if (rc != 0) {
+        ERROR_LOG("Fail. Relieasing mutex");    
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +51,26 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data *data = malloc(sizeof(struct thread_data));
+    if (data == NULL) {
+        return false;
+    }
+
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+
+    // Here we could perform some logic to simulate waiting before locking the mutex.
+    // For simplicity, we skip this and focus on mutex logic.
+
+    // Create the thread
+    if (pthread_create(thread, NULL, threadfunc, (void *)data) != 0) {
+        ERROR_LOG("Fail. Creating thread");
+        free(data);
+        return false;
+    }
+
+    return true;
 }
 
